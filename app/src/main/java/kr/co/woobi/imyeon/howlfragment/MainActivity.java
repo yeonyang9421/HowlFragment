59e273990fdc1b5fa1b269daed8c6fb22706ee42
yeonyang9421 <chris09+>
2018-11-18 23:15:57 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/woobi/imyeon/howlfragment/MainActivity.java
@@ -0,0 +1,48 @@
+package kr.co.woobi.imyeon.howlfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout menu1=(LinearLayout)findViewById(R.id.menu1);
+        LinearLayout menu2=(LinearLayout)findViewById(R.id.menu2);
+        LinearLayout menu3=(LinearLayout)findViewById(R.id.menu3);
+        LinearLayout menu4=(LinearLayout)findViewById(R.id.menu4);
+
+        menu1.setOnClickListener(this);
+        menu2.setOnClickListener(this);
+        menu3.setOnClickListener(this);
+        menu4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.menu1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MyFragement()).commit();
+
+                break;
+            case R.id.menu2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MyFragement2()).commit();
+                break;
+            case R.id.menu3:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MyFragement3()).commit();
+                break;
+            case R.id.menu4:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MyFragement4()).commit();
+                break;
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/kr/co/woobi/imyeon/howlfragment/MyFragement.java
@@ -0,0 +1,17 @@
+package kr.co.woobi.imyeon.howlfragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragement extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+}
